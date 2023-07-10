38e2c17e0d413880e29ef47b128d546a0d8194fb
Pratu <Pratu@DESKTOP-645JS17>
2023-07-10 14:19:55 +0530

    Add project files.

---
+++ Expense Tracker/Migrations/20230624102444_Added isAuthenticated bool.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker.Migrations
+{
+    public partial class AddedisAuthenticatedbool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAuthenticated",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAuthenticated",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "UserName");
+        }
+    }
+}
---
+++ Expense Tracker/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic;
+
+namespace Expense_Tracker.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username..")]
+        [Display(Name = "UserName")]
+        [Column(TypeName = "nvarchar(20)")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email..")]
+        [Display(Name = "UserEmail")]
+        [Column(TypeName = "nvarchar(20)")]
+        public string UserEmail { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Phone NO..")]
+        [Display(Name = "UserPhone")]
+        [Column(TypeName = "nvarchar(10)")]
+        public string UserPhone { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password...")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [Column(TypeName = "nvarchar(20)")]
+        public string UserPassword { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public bool IsAuthenticated { get; set; } = false;
+
+
+
+
+
+
+    }
+}
